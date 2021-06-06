fa900fb112d00b2fca1f42f7c8e9b16d22f2aa7e
Carlos <carlos.alfonso.montero@gmail.com>
2021-06-06 14:15:44 -0400

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,52 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toy, setToy] = useState([]);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function handleHide() {
+    setShowForm(false);
+  }
+
+  useEffect(() => (
+    fetch("http://localhost:3001/toys")
+    .then((response) => response.json())
+    .then((toysData) => setToy(toysData))
+  ), []);
+
+  function handleAddToy(newToy) {
+    const updatedToys = [...toy, newToy];
+    setToy(updatedToys);
+  }
+
+  function handleDeleteToy(id) {
+    const updatedToys = toy.filter((toyList) => toyList.id !== id);
+    setToy(updatedToys);
+  }
+
+  function handleUpdateToy(updatedToy) {
+    const updatedToys = toy.map((toyList) => {
+      if (toyList.id === updatedToy.id) {
+        return updatedToy;
+      } else {
+        return toyList;
+      }
+    });
+    setToy(updatedToys);
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
+        <button onClick={handleHide} >Hide Form</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toysList={toy} onDeletePlant={handleDeleteToy} onUpdateToy={handleUpdateToy} />
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toysList, onDeletePlant, onUpdateToy }) {  
+  function populateToysList() {
+    return toysList.map((toy) => (
+      <ToyCard toysList={toy} key={toy.id} onDeletePlant={onDeletePlant} onUpdateToy={onUpdateToy} />
+    ));
+  }
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{populateToysList()}</div>
   );
 }
 
